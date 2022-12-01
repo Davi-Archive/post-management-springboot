6535e8a248c0ad25e46acd7fc140f8911e1cb76e
d1av <davi4alves@gmail.com>
2022-12-01 00:21:46 -0300

    feat: add posts with user object

--- src/main/java/com/api/config/Instantiation.java
+++ src/main/java/com/api/config/Instantiation.java
@@ -1,31 +1,49 @@
 package com.api.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
 
+import com.api.domain.Post;
 import com.api.domain.User;
+import com.api.repositories.PostRepository;
 import com.api.repositories.UserRepository;
 
 // This class adds to the database if on first use.
 
-//@Configuration
+@Configuration
 public class Instantiation implements CommandLineRunner {
 
 	@Autowired
-	UserRepository repository;
+	UserRepository userRepository;
+
+	@Autowired
+	PostRepository postRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
-		repository.deleteAll();
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
+		userRepository.deleteAll();
 
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		User davi = new User(null, "Davi Alves", "davi@gmail.com");
 
-		repository.saveAll(Arrays.asList(maria, alex, bob,davi));
+		userRepository.saveAll(Arrays.asList(maria, alex, bob, davi));
+
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo, Abraços!",
+				maria);
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje.", maria);
+
+		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
 }
--- src/main/java/com/api/domain/Post.java
+++ src/main/java/com/api/domain/Post.java
@@ -3,23 +3,39 @@ package com.api.domain;
 import java.io.Serializable;
 import java.util.Date;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
 public class Post implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@Id
 	private String id;
 	private Date date;
 	private String title;
 	private String body;
+	private User author;
 
 	public Post() {
 
 	}
 
-	public Post(String id, Date date, String title, String body) {
+	public Post(String id, Date date, String title, String body, User author) {
 		this.id = id;
 		this.date = date;
 		this.title = title;
+		this.author = author;
 		this.body = body;
+	}
+
+	
+	public User getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(User author) {
+		this.author = author;
 	}
 
 	public String getId() {
