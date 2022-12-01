270aaac7536a74274e742ce2140fe7b84f547d50
d1av <davi4alves@gmail.com>
2022-12-01 14:43:41 -0300

    feat: add query with many params

--- src/main/java/com/api/resources/PostResource.java
+++ src/main/java/com/api/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.api.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,5 +33,17 @@ public class PostResource {
 		text = URL.decodeParam(text);
 		List<Post> posts = postService.findByTitle(text);
 		return ResponseEntity.status(200).body(posts);
+	}
+
+	@GetMapping(value = "/fullsearch")
+	public ResponseEntity<List<Post>> fullSearch(
+			@RequestParam(value = "text", defaultValue = "") String text,
+			@RequestParam(value = "minDate", defaultValue = "") String minDate,
+			@RequestParam(value = "text", defaultValue = "") String maxDate) {
+		text = URL.decodeParam(text);
+		Date min =URL.convertDate(minDate, new Date(0L));
+		Date max =URL.convertDate(maxDate, new Date());
+		List<Post> list = postService.fullSearch(text, min, max);
+		return ResponseEntity.status(200).body(list);
 	}
 }
--- src/main/java/com/api/services/PostService.java
+++ src/main/java/com/api/services/PostService.java
@@ -1,9 +1,11 @@
 package com.api.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import com.api.domain.Post;
@@ -25,8 +27,13 @@ public class PostService {
 	}
 
 	public List<Post> findByTitle(String text) {
-		//return postRepository.findByTitleContainingIgnoreCase(text);
+		// return postRepository.findByTitleContainingIgnoreCase(text);
 		return postRepository.searchTitle(text);
+	}
+
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return postRepository.fullSearch(text, minDate, maxDate);
 	}
 
 }
